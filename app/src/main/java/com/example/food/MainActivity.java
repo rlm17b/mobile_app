e65fc1e40058b535912333e0b7cb20a6252a83b2
robert <robmellinger13@gmail.com>
2021-03-10 23:16:41 -0500

    added sign in screen template and onclick function

--- app/src/main/java/com/example/food/MainActivity.java
+++ app/src/main/java/com/example/food/MainActivity.java
@@ -4,18 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signin_layout);
-        final EditText editText = new EditText(this);
-        String email = editText.getText().toString();
-        Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
-        intent.putExtra("mail", email);
-        //startActivity(intent);
+
+        EditText eMail = (EditText)findViewById(R.id.email);
+        submit = (Button)findViewById(R.id.enter);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    String email = eMail.getText().toString();
+                    if (valid(email) == true) {
+                        Toast.makeText(getApplicationContext(), "Welcome to the Food app", Toast.LENGTH_SHORT).show();
+                        setContentView(R.layout.activity_main);
+                    } else {
+                        Toast.makeText(getApplicationContext(), "This is an invalid email address", Toast.LENGTH_SHORT).show();
+                    }
+
+            }
+        });
+
+    }
+
+    private boolean valid(String emailID){
+        /*if(emailID ==){
+            return true;
+        }
+        else{
+            return false;
+        }*/
+        return true;
     }
 }
