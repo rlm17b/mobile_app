14256a6353c5473312c80de4d4467873f0e5f366
robert <robmellinger13@gmail.com>
2021-03-10 22:30:51 -0500

    added sign in screen template

--- app/src/main/java/com/example/food/MainActivity.java
+++ app/src/main/java/com/example/food/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.food;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.signin_layout);
+        final EditText editText = new EditText(this);
+        String email = editText.getText().toString();
+        Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+        intent.putExtra("mail", email);
+        //startActivity(intent);
     }
 }
